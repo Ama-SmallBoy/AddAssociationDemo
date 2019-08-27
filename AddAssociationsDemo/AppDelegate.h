1b0da68f2d7cd2d24ff59cc662cbc9805083e3ec
gaotong zhang <zhanggaotong2@gmail.com>
2019-08-27 20:05:13 +0800

    Initial Commit

---
+++ AddAssociationsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AddAssociationsDemo
+//
+//  Created by a on 2019/8/27.
+//  Copyright © 2019年 TeenageBeaconFireGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AddAssociationsDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AddAssociationsDemo
+//
+//  Created by a on 2019/8/27.
+//  Copyright © 2019年 TeenageBeaconFireGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
